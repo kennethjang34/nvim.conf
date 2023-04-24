b352008704e70ab560883d73f843bd622ff4ac64
JANG <kennethjang34@gmail.com>
2023-04-23 21:34:56 -0700

    add keymaps for lazygit and jupyter ascending

--- lua/plugins/jupyter_ascending.lua
+++ lua/plugins/jupyter_ascending.lua
@@ -3,9 +3,22 @@ return {
 	config = function()
 		--require('jupyter_ascending').setup()
 		local keyset = vim.keymap.set
-		keyset('n', '<leader>x', '<Plug>JupyterExecute', { noremap = true, silent = true })
-		keyset('n', '<leader>S', '<Plug>JupyterExecute', { noremap = true, silent = true })
-	end,
+		-- keyset('n', '<leader>jx', '<Plug>JupyterExecute', { noremap = true, silent = true })
+		-- keyset('n', '<leader>jX', '<Plug>JupyterExecute', { noremap = true, silent = true })
+		local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+		local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+		augroup('JupyterAscending', { clear = true })
+		autocmd('BufRead,BufNewFile', {
+			group = 'JupyterAscending',
+			pattern = "*.sync.py",
+			callback = function()
+				vim.api.nvim_buf_set_keymap(0, 'n', '<leader>jx', '<Plug>JupyterExecute',
+					{ noremap = true, silent = true })
+				vim.api.nvim_buf_set_keymap(0, 'n', '<leader>jX', '<Plug>JupyterExecuteAll',
+					{ noremap = true, silent = true })
+			end
+		})
+	end
 	--nnoremap <space>x <Plug>JupyterExecute
 	--nnoremap <LocalLeader>X <Plug>JupyterExecuteAll
 }
