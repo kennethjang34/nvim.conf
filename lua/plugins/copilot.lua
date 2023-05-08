3a258d2821203a47cd401b59709e630847c733a7
JANG <kennethjang34@gmail.com>
2023-05-08 07:28:25 -0700

    add next, prev selection for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,9 +4,9 @@ return {
 		vim.g.copilot_no_tab_map = true
 		vim.keymap.set("i", "<c-l>", 'copilot#Accept("")',
 			{ noremap = true, silent = true, expr = true, replace_keycodes = false })
+		vim.keymap.set("i", "<c-p>", '<Plug>(copilot-previous)',
+			{ noremap = true, silent = true, expr = false, replace_keycodes = false })
+		vim.keymap.set("i", "<c-n>", '<Plug>(copilot-next)',
+			{ noremap = true, silent = true, expr = false, replace_keycodes = false })
 	end,
-	-- inoremap <silent><script><expr> <Right> copilot#Accept("")
-	-- inoremap <silent> <C-p> <Plug>(copilot-previous)
-	-- inoremap <silent> <C-n> <Plug>(copilot-next)
-	-- let g:copilot_no_tab_map = v:true
 }
