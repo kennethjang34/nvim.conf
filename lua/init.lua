87ca1e037c668795dd54ceb509cf66605dcfd3b7
JANG <kennethjang34@gmail.com>
2023-06-11 08:31:13 -0700

    add vimwiki plugin

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,7 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copiot_tab_map = ""
+vim.g.vimwiki_key_mappings = { all_maps = 0 }
 vim.g.mapleader = " "
 vim.g.coc_node_path = '$HOME/.nvm/versions/node/v18.9.0/bin/node'
 require('load_lazy')
---
+++ lua/plugins/vimwiki.lua
@@ -0,0 +1,33 @@
+return {
+	'vimwiki/vimwiki',
+	config = function()
+		local keyset = vim.keymap.set
+		-- vim.g.vimwiki_key_mappings = { all_maps = 1, global = 1 }
+		keyset('n', '<leader>ww', '<Plug>VimwikiIndex', { noremap = true, silent = true })
+		keyset('n', '<leader>wt', '<Plug>VimwikiTabIndex', { noremap = true, silent = true })
+		keyset('n', '<leader>ws', '<Plug>VimwikiUISelect', { noremap = true, silent = true })
+		local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+		local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+		augroup('VimWiki', { clear = true })
+		autocmd('BufRead,BufNewFile', {
+			group = 'VimWiki',
+			pattern = "*.wiki",
+			callback = function()
+				vim.api.nvim_buf_set_keymap(0, 'n', '<leader>ll', '<Plug>VimwikiBaddLink',
+					{ noremap = true, silent = true })
+				vim.api.nvim_buf_set_keymap(0, 'n', 'gd', '<Plug>VimwikiFollowLink',
+					{ noremap = true, silent = true })
+				vim.api.nvim_buf_set_keymap(0, 'n', '<leader>ln', '<Plug>VimwikiNextLink',
+					{ noremap = true, silent = true })
+				vim.api.nvim_buf_set_keymap(0, 'n', '<leader>lp', '<Plug>VimwikiPrevLink',
+					{ noremap = true, silent = true })
+			end
+		})
+		-- vim.g.db_ui_save_location = '~/.config/db_ui'
+		-- vim.g.db_ui_execute_on_save = 1
+		-- keyset('n', '<leader>du', '<cmd>DBUIToggle<CR>', { noremap = true })
+		-- keyset('n', '<leader>df', '<cmd>DBUIFindBuffer<CR>', { noremap = true })
+		-- keyset('n', '<leader>dr', '<cmd>DBUIRenameBuffer<CR>', { noremap = true })
+		-- keyset('n', '<leader>dl', '<cmd>DBUILastQueryInfo<CR>', { noremap = true })
+	end
+}
